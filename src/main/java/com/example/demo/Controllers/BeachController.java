22e5ebe7c9d3f7c159ffc8ee0d2dceba43b6fac0
Tom Grant <grntthomasronnie@gmail.com>
2023-05-01 16:26:19 +0100

    Got beaches working api

--- src/main/java/com/example/demo/Controllers/BeachController.java
+++ src/main/java/com/example/demo/Controllers/BeachController.java
@@ -1,4 +1,31 @@
 package com.example.demo.Controllers;
+import com.example.demo.Models.Beach;
+import com.example.demo.Repositories.BeachRepository;
+import com.example.demo.Repositories.CreatureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class BeachController {
+
+    @Autowired
+    BeachRepository beachRepository;
+
+    @Autowired
+    CreatureRepository creatureRepository;
+
+    @GetMapping(value = "/beaches")
+    public List<Beach> getAllBeaches() {
+        return beachRepository.findAll();
+    }
+
+    @GetMapping(value="/beaches/{id}")
+    public Optional<Beach> getBeach(@PathVariable Long id){
+        return beachRepository.findById(id);
+    }
 }
