6b8cf2d6c2217a58b543407f5d2ced4010d3156b
SShingler <s.shingler@hotmail.co.uk>
2023-05-04 15:49:45 +0100

    various styling/layout

--- capstone_project/src/Components/SeaLife/SeaLifeCard.js
+++ capstone_project/src/Components/SeaLife/SeaLifeCard.js
@@ -11,12 +11,26 @@ const SeaLifeCard = ({ creature }) => {
     return (
         <>
             <div className="sea-life-card">
-                <p> Type: '{creature.type}' </p>
+                <div className="card-border">
+                    <div className="card-front">
+                        <div className="card-image">
+                        <img src = {require("../../Images/Creatures/" + creature.img)} alt = {`A picture of a ${creature.name}`} height="280" width="350" style={{ objectFit: "fill" }}></img>
+                        </div>
+                        <p className="creature-name"> {creature.name}</p>
+                        <p className="creature-type"> {creature.type} - {creature.size} </p>
+                        <p className="creature-habitat"> {creature.habitat}</p>
+                        <p className="creature-notes"> {creature.notes}</p>
+                    </div>
+
+
+
+                </div>
+                {/* <p> Type: '{creature.type}' </p>
                 <p> Name: {creature.name}</p>
                 <img className="img-size" src = {require("../../Images/Creatures/" + creature.img)} alt = {`A picture of a ${creature.name}`}></img>
                 <p> Size: {creature.size}</p>
                 <p> Habitat: {creature.habitat}</p>
-                <p> Notes: {creature.notes}</p>
+                <p> Notes: {creature.notes}</p> */}
             </div>
         </>
     );
